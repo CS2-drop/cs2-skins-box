3b54d5e82504b7b2d653affb4b072c891bc4d0ac
CS2-drop <dimavasilishin78@gmail.com>
2025-05-25 10:29:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const cases = document.querySelectorAll('.case');
+
+  cases.forEach(caseEl => {
+    caseEl.addEventListener('click', () => {
+      if (!caseEl.classList.contains('open')) {
+        caseEl.classList.add('open');
+      }
+    });
+  });
+});
 
