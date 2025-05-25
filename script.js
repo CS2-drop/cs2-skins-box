21d57200c94cd8853b421520d96a037b52a5ddc0
CS2-drop <dimavasilishin78@gmail.com>
2025-05-25 10:45:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ document.addEventListener("DOMContentLoaded", function() {
   var openBtn = document.getElementById("open-box-btn");
   var modal = document.getElementById("modal");
   var animationBox = document.getElementById("animation-box");
+  var caseImg = document.getElementById("case-img");
   var closeBtn = document.getElementById("close-modal");
-
+  var cases = [
+    "cs2-skins-box/images/cases/case1.jpg",
+    "cs2-skins-box/images/cases/case2.jpg",
+    "cs2-skins-box/images/cases/case3.jpg"
+  ];
   openBtn.addEventListener("click", function() {
     modal.style.display = "flex";
+    var randomIndex = Math.floor(Math.random() * cases.length);
+    caseImg.src = cases[randomIndex];
     setTimeout(function() {
       animationBox.classList.add("open");
     }, 100);
   });
-
   closeBtn.addEventListener("click", function() {
     modal.style.display = "none";
     animationBox.classList.remove("open");
   });
-
   modal.addEventListener("click", function(e) {
     if (e.target === modal) {
       modal.style.display = "none";
